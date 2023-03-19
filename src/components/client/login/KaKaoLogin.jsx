d5b9156222cb04759547d2396f26b5e488bcb0c6
HwangInUng <ung6860@naver.com>
2023-03-20 02:15:50 +0900

    chore: kakao login 오류 해결 -login 페이지 출력없이 redirect되는 문제 식별 -최초 요청 시 prompt값 + scope값 지정 -login완료 후 spring 서버로 토큰 전달 정상 확인

--- src/components/client/login/KaKaoLogin.jsx
+++ src/components/client/login/KaKaoLogin.jsx
@@ -9,9 +9,9 @@ function KakaoLogin() {
   const client_id = process.env.REACT_APP_KAKAO_CLIENT_ID;
   const redirect_uri = process.env.REACT_APP_KAKAO_REDIRECT_URI;
 
-  const url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}`;
+  const url = `https://kauth.kakao.com/oauth/authorize?scope=account_email&client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&prompt=login`;
 
-  const loginKaKao = async () => {
+  const loginKaKao = () => {
     window.location.href=url;
   }
 
